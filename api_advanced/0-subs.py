6bb3029f62413a7bb98d8957af8c08e33b21ff4d
Q-Reine <q.wihogora@alustudent.com>
2024-07-03 04:00:37 -0700

    API advanced

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -11,7 +11,9 @@ def number_of_subscribers(subreddit):
     Returns the number of subscribers for a given subreddit
     """
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
+    headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+            }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
         return 0
