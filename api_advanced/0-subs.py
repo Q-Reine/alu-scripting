2d6b31f7c7036b6918060cde1721cf5393ef3be5
Q-Reine <q.wihogora@alustudent.com>
2024-07-04 23:49:53 +0200

    How many subs?

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -4,13 +4,16 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    Returns the number of subscribers for a given subreddit
-    """
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "LloydBot3.2.12 (by /akumuyi)"
+    }
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
+    if response.status_code == 404:
         return 0
-    data = response.json().get('data')
-    return data.get('subscribers')
+    elif response.status_code == 200:
+        results = response.json().get("data", {})
+        return results.get("subscribers", 0)
+    else:
+        return 0
