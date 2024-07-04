5db79e4b9863ebe1f8fbe979691347bf664d0272
Q-Reine <q.wihogora@alustudent.com>
2024-07-05 00:05:03 +0200

    How many subs?

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,28 +1,17 @@
 #!/usr/bin/python3
-"""
-Defines function that queries the Reddit API and returns the
-number of subscribers
-"""
+"""Return the number of subscribers of a given subreddit"""
+
 import requests
-import sys
 
 
 def number_of_subscribers(subreddit):
-    """Queries the Reddit API and returns the
-    number of subscribers
-    Return:
-        0 - if invalid subreddit is given
-    """
-    if subreddit is None or not isinstance(subreddit, str):
-        return(0)
-    endpoint = 'https://www.reddit.com'
-    headers = {
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '}
-    info = requests.get('{}/r/{}/about.json'.format(
-            endpoint,
-            subreddit), headers=headers, allow_redirects=False)
-    if info.status_code == 200:
-        json_info = info.json()
-        return(json_info.get('data').get('subscribers'))
-    else:
-        return(0)
+    """function that fetches number_of_subscribers"""
+    URL = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
+
+    try:
+        RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
+        return RESPONSE.json().get("data").get("subscribers")
+
+    except Exception:
+        return 0
