6b1face39ddf41c426e57fb52e4f32f6533a6b49
Q-Reine <q.wihogora@alustudent.com>
2024-07-05 00:26:20 +0200

    How many subs?

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,26 +1,15 @@
 #!/usr/bin/python3
-"""Return the number of subscribers of a given subreddit"""
-
+"""Return number of subscribers for a given subreddit"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    Returns the number of subscribers for a given subreddit
-    """
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
-        return 0
-    data = response.json().get('data')
-    return data.get('subscribers')
-
-
-if __name__ == '__main__':
-    import sys
-
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        print("{:d}".format(number_of_subscribers(sys.argv[1])))
+    """Return the number of subscribers """
+    url = "https://www.reddit.com/r/{}/about.json" \
+        .format(subreddit)
+    headers = {'User-Agent': 'My User Agent 1.0'}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return response.json().get('data') \
+            .get('subscribers')
+    return 0
