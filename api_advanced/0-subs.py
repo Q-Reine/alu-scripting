3de24e5f94ffb2931d776a9473a5971869e5ee1c
Q-Reine <q.wihogora@alustudent.com>
2024-07-06 05:11:28 -0700

    How many subs?

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -12,9 +12,7 @@ def number_of_subscribers(subreddit):
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    
     if response.status_code != 200:
         return 0
-    
     data = response.json().get('data')
     return data.get('subscribers')
