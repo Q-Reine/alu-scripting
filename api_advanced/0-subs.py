10cd1bb43417d885174c64f682a5acd02738b8f5
Q-Reine <q.wihogora@alustudent.com>
2024-07-06 05:21:34 -0700

    How many subs?

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,19 +1,28 @@
 #!/usr/bin/python3
-"""
-Query the Reddit API to get the number of subscribers for a given subreddit
-"""
-
+'''
+Defines function that queries the Reddit API and returns the
+number of subscribers
+'''
 import requests
+import sys
 
 
 def number_of_subscribers(subreddit):
-    """
-    Returns the number of subscribers for a given subreddit
-    """
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
-        return 0
-    data = response.json().get('data')
-    return data.get('subscribers')
+    '''Queries the Reddit API and returns the
+    number of subscribers
+    Return:
+        0 - if invalid subreddit is given
+    '''
+    if subreddit is None or not isinstance(subreddit, str):
+        return(0)
+    endpoint = 'https://www.reddit.com'
+    headers = {'user-agent': 'Mozilla/5.0 \
+(Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0'}
+    info = requests.get('{}/r/{}/about.json'.format(
+            endpoint,
+            subreddit), headers=headers, allow_redirects=False)
+    if info.status_code == 200:
+        json_info = info.json()
+        return(json_info.get('data').get('subscribers'))
+    else:
+        return(0)
