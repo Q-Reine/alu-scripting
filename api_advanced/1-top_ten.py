94843bee9c75b16667e072b7be0e5a3b0637d36f
Q-Reine <q.wihogora@alustudent.com>
2024-07-11 09:58:31 -0700

    Top Ten

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -13,11 +13,17 @@ def top_ten(subreddit):
     """Prints the top ten hot posts for a given subreddit"""
 
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-     headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
+    response = requests.get(url, headers=headers)
+
     if response.status_code != 200:
         print(None)
         return
-    posts = response.json()['data']['children']
-    for post in posts:
-        print(post['data']['title'])
+
+    data = response.json().get("data")
+    if data is None or len(data.get("children")) == 0:
+        print(None)
+        return
+
+    for child in data.get("children"):
+        print(child.get("data").get("title"))
